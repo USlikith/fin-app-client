f54ad63ce9088487d480cf6c7904961d3bfce5f6
USlikith <likithus2@gmail.com>
2024-09-16 20:06:15 +0530

    this is the initial commit of the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css'
+import NavBar from './compoents/headder/headder.jsx'
+
+function App() {
+
+
+  return (
+     <>
+      <div className='outerBox' style={{maxWidth:'3000px'}}>
+
+     <NavBar />
+      </div>
+     </>
+  )
+}
+
+export default App
